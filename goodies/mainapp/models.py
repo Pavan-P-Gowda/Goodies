92fb60a4a48b619aea8a05dfb7c8aaf73afe9b89
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-03-22 12:38:19 +0530

    created models and admin

--- goodies/mainapp/models.py
+++ goodies/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class   Product(models.Model):
+    name = models.CharField(max_length=200) #in db it is varchar
+    price = models.FloatField() # in db it is int
+    desc = models.TextField(max_length=500)
+    img = models.ImageField(upload_to='products/')
+    stock = models.PositiveIntegerField()
