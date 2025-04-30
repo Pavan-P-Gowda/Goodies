8c3f5e25f24f0b89fc5e6603fa8831dde5f97d88
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-04-30 07:45:14 +0530

    edited app name

--- uDrive/uDrive/asgi.py
+++ uDrive/uDrive/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for goodies project.
+ASGI config for uDrive project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uDrive.settings')
 
 application = get_asgi_application()
