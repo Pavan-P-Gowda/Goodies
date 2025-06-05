addbe75841267d3d131fd1a2ce2c7c75c508ea72
Pavan-P-Gowda <pavanpgowda31@gmail.com>
2025-06-05 08:48:43 +0530

    edited in history

--- uDrive/rent/urls.py
+++ uDrive/rent/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns =[
     # path('car/<int:car_id>/add/', views.addRent, name='addrent'),
-    path('rent/<int:car_id>/', views.rentView, name='rentcart'),
-    path('rent/<int:car_id>', views.updateCar, name = 'update_car')
+    path('add/<int:car_id>/', views.rentView, name='rentcart'),
+    path('update/<int:car_id>', views.updateCar, name = 'update_car')
 ]
